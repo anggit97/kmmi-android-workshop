0c262a118207eb305bc9af7c659a6a21e56b4ca0
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-18 15:58:02 +0700

    Initialize project

---
+++ app/src/main/java/com/anggit97/kmmiblog/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.anggit97.kmmiblog.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anggit97.kmmiblog.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
