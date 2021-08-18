4b11ff2cab6743e27abc2894720f1682c2e14669
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-18 17:23:07 +0700

    Jalankan aplikasi

--- app/src/main/java/com/anggit97/kmmiblog/ui/splash/SplashActivity.java
+++ app/src/main/java/com/anggit97/kmmiblog/ui/splash/SplashActivity.java
@@ -1,15 +1,36 @@
 package com.anggit97.kmmiblog.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.anggit97.kmmiblog.R;
+import com.anggit97.kmmiblog.ui.main.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final long DELAY_DURATION = 2000L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigateToMainActivity();
+            }
+        }, DELAY_DURATION);
+    }
+
+    private void navigateToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
