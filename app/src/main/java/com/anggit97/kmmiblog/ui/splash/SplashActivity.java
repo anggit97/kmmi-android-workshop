750a7d269eee905ac7a3e830fd074e7e3d20344b
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-18 16:37:06 +0700

    Membuat splash activity dan persiapan assets

---
+++ app/src/main/java/com/anggit97/kmmiblog/ui/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.anggit97.kmmiblog.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anggit97.kmmiblog.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
