a560584d71e5a188078d6ba0a501839197cc01b4
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-22 13:50:12 +0700

    Membuat Desain Create/edit my news

---
+++ app/src/main/java/com/anggit97/kmmiblog/ui/createedit/CreateEditActivity.java
@@ -0,0 +1,16 @@
+package com.anggit97.kmmiblog.ui.createedit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anggit97.kmmiblog.R;
+
+public class CreateEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_edit);
+    }
+}
