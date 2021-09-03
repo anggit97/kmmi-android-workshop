00bee066d9007c367554992fb7767c9c65c44dfa
Anggit Prayogo <anggitprayogo@gmail.com>
2021-09-03 14:41:25 +0700

    Menampilkan List di Halaman Favorite

---
+++ app/src/main/java/com/anggit97/kmmiblog/ui/favorite/FavoriteAdapter.java
@@ -0,0 +1,103 @@
+package com.anggit97.kmmiblog.ui.favorite;
+
+import static com.anggit97.kmmiblog.ui.detail.DetailActivity.DATA_EXTRA_KEY;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.anggit97.kmmiblog.R;
+import com.anggit97.kmmiblog.api.model.Post;
+import com.anggit97.kmmiblog.db.entity.PostEntity;
+import com.anggit97.kmmiblog.ui.detail.DetailActivity;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Anggit Prayogo on 03,September,2021
+ * GitHub : https://github.com/anggit97
+ */
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder> {
+
+    private final List<PostEntity> listPost = new ArrayList<>();
+
+    void setListPost(List<PostEntity> listPost){
+        this.listPost.clear();
+        this.listPost.addAll(listPost);
+        this.notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public FavoriteAdapter.FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_news, parent, false);
+        return new FavoriteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteAdapter.FavoriteViewHolder holder, int position) {
+        holder.bindItem(listPost.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listPost.size();
+    }
+
+    public static class FavoriteViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView ivThumbnail;
+        private final TextView tvTitle;
+        private final TextView tvDate;
+        private ImageView ivFavorite;
+
+        public FavoriteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivThumbnail = itemView.findViewById(R.id.ivThumbnailNews);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvDate = itemView.findViewById(R.id.tvDateNews);
+            ivFavorite = itemView.findViewById(R.id.ivFavorite);
+        }
+
+        public void bindItem(PostEntity postEntity) {
+            Post post = new Post();
+            post.setId(postEntity.getId());
+            post.setTitle(postEntity.getTitle());
+            post.setBody(postEntity.getBody());
+            post.setCreatedAt(postEntity.getCreatedAt());
+            post.setUpdatedAt(postEntity.getUpdatedAt());
+            post.setThumbnailUrl(postEntity.getImagePath());
+
+            tvTitle.setText(post.getTitle());
+            tvDate.setText(post.getCreatedAt());
+
+            Glide.with(itemView.getContext())
+                    .load(post.getThumbnailUrl())
+                    .into(ivThumbnail);
+
+            if (post.isFavorite()) {
+                ivFavorite.setImageDrawable(ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_baseline_favorite_24));
+            } else {
+                ivFavorite.setImageDrawable(ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_baseline_favorite_border_24));
+            }
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(itemView.getContext(), DetailActivity.class);
+                    intent.putExtra(DATA_EXTRA_KEY, post);
+                    itemView.getContext().startActivity(intent);
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/anggit97/kmmiblog/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/anggit97/kmmiblog/ui/favorite/FavoriteFragment.java
@@ -4,12 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.anggit97.kmmiblog.R;
+import com.anggit97.kmmiblog.db.BlogDatabase;
+import com.anggit97.kmmiblog.db.entity.PostEntity;
+import com.anggit97.kmmiblog.util.AppExecutors;
+
+import java.util.List;
 
 public class FavoriteFragment extends Fragment {
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView rvFavorite;
+    private ProgressBar pbLoading;
+    private TextView tvFavoriteEmpty;
+
+    private BlogDatabase db;
+
+    private FavoriteAdapter adapter;
 
     public static FavoriteFragment newInstance() {
         return new FavoriteFragment();
@@ -18,7 +39,66 @@ public class FavoriteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        rvFavorite = view.findViewById(R.id.rvFavorite);
+        pbLoading = view.findViewById(R.id.pbLoading);
+        tvFavoriteEmpty = view.findViewById(R.id.tvFavoriteEmpty);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        adapter = new FavoriteAdapter();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        rvFavorite.setLayoutManager(linearLayoutManager);
+        rvFavorite.setAdapter(adapter);
+
+        db = BlogDatabase.getInstance(getContext());
+
+        onActionListener();
+    }
+
+    private void onActionListener() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchData();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fetchData();
+    }
+
+    private void fetchData() {
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                List<PostEntity> postList = db.postDao().getFavoritePost();
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.setListPost(postList);
+                        if (postList.isEmpty()){
+                            tvFavoriteEmpty.setVisibility(View.VISIBLE);
+                            rvFavorite.setVisibility(View.GONE);
+                        }else{
+                            tvFavoriteEmpty.setVisibility(View.GONE);
+                            rvFavorite.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+            }
+        });
     }
 }
