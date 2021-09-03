000e76b418d81fc74dd29861f9f9236f7c187c3d
Anggit Prayogo <anggitprayogo@gmail.com>
2021-09-03 10:39:08 +0700

    Implementasi Fitur Menambahkan News Ke Favorit di Halaman Detail.

--- app/src/main/java/com/anggit97/kmmiblog/ui/detail/DetailActivity.java
+++ app/src/main/java/com/anggit97/kmmiblog/ui/detail/DetailActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.anggit97.kmmiblog.R;
 import com.anggit97.kmmiblog.api.model.Post;
+import com.anggit97.kmmiblog.db.BlogDatabase;
+import com.anggit97.kmmiblog.db.entity.PostEntity;
+import com.anggit97.kmmiblog.ui.main.MainActivity;
+import com.anggit97.kmmiblog.util.AppExecutors;
 import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
@@ -24,6 +29,7 @@ public class DetailActivity extends AppCompatActivity {
     private ImageView ivBack;
     private ImageView ivFavorite;
     private TextView tvDate;
+    private BlogDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +43,13 @@ public class DetailActivity extends AppCompatActivity {
         ivBack = findViewById(R.id.ivBack);
         ivFavorite = findViewById(R.id.ivFavorite);
 
+        db = BlogDatabase.getInstance(this);
+
         handleIntent();
 
         onActionClick();
+
+        fetchFavorite();
     }
 
     private void onActionClick() {
@@ -47,6 +57,86 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        ivFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int count = db.postDao().checkIfPostIsFavorite(post.getId());
+                        if (count >= 1) {
+                            removeFromFavorite();
+                        } else {
+                            addToFavorite();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void removeFromFavorite(){
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                PostEntity postEntity = new PostEntity(
+                        post.getId(),
+                        post.getTitle(),
+                        post.getBody(),
+                        post.getCreatedAt(),
+                        post.getUpdatedAt(),
+                        post.getThumbnailUrl()
+                );
+                db.postDao().removeFromFavorite(postEntity);
+                fetchFavorite();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(DetailActivity.this, "remove to favorite successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void addToFavorite() {
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                PostEntity postEntity = new PostEntity(
+                        post.getId(),
+                        post.getTitle(),
+                        post.getBody(),
+                        post.getCreatedAt(),
+                        post.getUpdatedAt(),
+                        post.getThumbnailUrl()
+                );
+                db.postDao().addToFavorite(postEntity);
+                fetchFavorite();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(DetailActivity.this, "Add to favorite successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void fetchFavorite() {
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                int count = db.postDao().checkIfPostIsFavorite(post.getId());
+                if (count >= 1) {
+                    ivFavorite.setImageDrawable(ContextCompat.getDrawable(DetailActivity.this, R.drawable.ic_baseline_favorite_24));
+                } else {
+                    ivFavorite.setImageDrawable(ContextCompat.getDrawable(DetailActivity.this, R.drawable.ic_baseline_favorite_border_24));
+                }
             }
         });
     }
